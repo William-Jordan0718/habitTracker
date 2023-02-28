e8ba03dba853e50f7538b00d598e65c04a0ae3b9
williamjordan <will.jordan0788@gmail.com>
2023-02-28 15:49:35 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+from datetime import datetime
+
+USERNAME = "williamj"
+
+TOKEN = "michelin1"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+GRAPH_ID = "graph"
+
+user_params = {
+    "token": "michelin1",
+    "username": "williamj",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Running Graph",
+    "unit": "miles",
+    "type": "float",
+    "color": "ichou"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+today = datetime.now()
+print(today.strftime("%Y%m%d"))
+
+body = {
+    "date": today.strftime("%Y%m%d"),
+    "quantity": "6.0",
+}
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+pixel_creation_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+# response = requests.post(url=pixel_creation_endpoint, json=body, headers=headers)
+# print(response.text)
+
+update_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+new_pixel_data = {
+    "quantity": "15"
+}
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
+
+delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
+
+response = requests.delete(url=delete_endpoint, headers=headers)
+print(response.text)
